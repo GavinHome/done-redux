59af0d747037c52ff68badb58fa627174fb51d4f
GavinHome <yangxiaoming512@yeah.net>
2022-11-16 15:48:06 +0800

    refractor

--- lib/src/redux/dependencies/basic.cs
+++ lib/src/redux/dependencies/basic.cs
@@ -1,16 +1,5 @@
 ﻿using Redux.Adapter;
 using Redux.Basic;
-using Redux.Connector;
-using Redux.Effect;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Intrinsics.Arm;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Collections.Specialized.BitVector32;
-using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Redux.Dependencies.Basic;
 
