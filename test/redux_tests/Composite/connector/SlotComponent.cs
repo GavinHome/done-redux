078c4320cfcb156cdb3007a8f9927c3d1a98f9ac
GavinHome <yangxiaoming512@yeah.net>
2022-11-17 13:06:58 +0800

    add package settings

--- test/redux_tests/Composite/connector/SlotComponent.cs
+++ test/redux_tests/Composite/connector/SlotComponent.cs
@@ -15,7 +15,9 @@ internal class MessageComponent : Redux.Component<Message.MessageState>
     public MessageComponent() : base(
      //view: buildView,
      //effect: buildEffect(),
+#pragma warning disable CS8625 // 无法将 null 字面量转换为非 null 的引用类型。
      reducer: Message.MessageReducer.buildReducer(), null
+#pragma warning restore CS8625 // 无法将 null 字面量转换为非 null 的引用类型。
    )
     { }
 }
